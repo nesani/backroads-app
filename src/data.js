b271b6099f0bbffb4be961d185f92320283feb38
Nenad <nesani@ymail.com>
2023-04-04 13:21:49 +0200

    - added data.js - added loop for links in NavBar

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+    { id:1, href:'#home', text:"home" },
+    { id:2, href:'#about', text:"about" },
+    { id:3, href:'#services', text:"services" },
+    { id:4, href:'#tours', text:"tours" },
+]
+
+export const socialLinks = [
+    { id:1, href:'https://www.facebook.com', target:"_blank", ico: "fab fa-facebook" },
+    { id:2, href:'https://www.twitter.com', target:"_blank", ico: "fab fa-twitter" },
+    { id:3, href:'https://www.instagram.com', target:"_blank", ico: "fab fa-instagram" },
+]
